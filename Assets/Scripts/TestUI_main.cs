2556aef97b1c7b394e63fd80eab2a98acee1126a
nil-tyou <nil_tyou@sina.com>
2013-05-16 21:00:16 +0800

    add main

---
+++ Assets/Scripts/TestUI_main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUI_main : MonoBehaviour {
+
+//	public Texture2D texBG;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		GUI.Box (new Rect(0,0,100,40), "Main");
+	}
+}
